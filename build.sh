533d0f1ebff433df1bbc1e6b6fe1ed1890893ec6
root <root@localhost.localdomain>
2008-06-03 13:24:25 -0600

    added the default.conf and the required changes in build.sh and live-config.ks.in

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 GPG_KEY=$SCRIPT_DIR/RPM-GPG-KEY-PGuay.txt
 
+source $SCRIPT_DIR/default.conf
+
+if [ "$1" = "--config" -a -e "$2" ]; then
+	source $2
+fi
+
 import_key() {
     email=$(gpg -v $GPG_KEY 2>/dev/null  | grep 1024D | perl -p -i -e 's/.*<(.*)>/\1/')
 
@@ -23,7 +29,24 @@ import_key() {
 rm -rf $SCRIPT_DIR/livecd 
 mkdir -p $SCRIPT_DIR/livecd
 
-perl -p -e "s|##SCRIPT_DIR##|$SCRIPT_DIR|g;" $SCRIPT_DIR/livecd-config.ks.in > $SCRIPT_DIR/livecd-config.ks
+	perl -p -e "s|##SCRIPT_DIR##|$SCRIPT_DIR|g;" $SCRIPT_DIR/livecd-config.ks.in > $SCRIPT_DIR/livecd-config.ks
+
+for varname in 			\
+	CENTOS_RELEASED_URL	\
+	CENTOS_UPDATES_URL	\
+	CENTOS_ADDONS_URL	\
+	CENTOS_EXTRAS_URL	\
+	CENTOS_PLUS_URL		\
+	CENTOS_FAST_URL		\
+	DELL_HARDWARE_REPO_URL	\
+	DELL_SOFTWARE_REPO_URL	\
+	DELL_FIRMWARE_REPO_URL
+do
+	cp $SCRIPT_DIR/livecd-config.ks $SCRIPT_DIR/livecd-config.tmp
+	perl -p -e "s|##$varname##|${!varname}|g;" $SCRIPT_DIR/livecd-config.tmp> $SCRIPT_DIR/livecd-config.ks
+done
+
+rm $SCRIPT_DIR/livecd-config.tmp
 
 createrepo $SCRIPT_DIR/repository
 import_key
