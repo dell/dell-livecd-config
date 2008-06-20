651357937bb6a6b3770e3050245dfbe38db5737e
root <root@localhost.localdomain>
2008-06-20 14:27:32 -0600

    xml parser for getting the firmware packages.

---
+++ extract_rpms_xml.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use XML::Parser;
+
+my $filename = shift ;
+
+open(XMLFILE,$filename);
+my $tag;
+my $parser;
+print "%packages\n";
+$parser = new XML::Parser(ErrorContext => 2);
+$parser->setHandlers(Start => \&start_handler, Char => \&char_handler, End => \&end_handler);
+$parser->parsefile($filename);
+
+print "%end\n";
+
+sub start_handler()
+{
+  my $parse = shift;
+     $tag = shift;
+  #print "\nstart : $tag"	
+}
+
+sub char_handler()
+{
+	
+  my ($parse,$data)= @_;
+#	print "char $data\t ";
+	if($tag eq "name") { print "$data";}
+	#$tag = "none";
+} 
+
+sub end_handler()
+{
+  my ($parse,$tag)= @_;
+	if($tag eq "name"){ print "\n";}
+}
